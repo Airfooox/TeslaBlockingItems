eb3526de7270d2de1422dfa088abc7030ac26c26
Airfox <konstantin.zisiadis@st.ovgu.de>
2020-10-25 18:51:11 +0100

    feat: add scp268 support

--- TeslaBlockingItems/Config.cs
+++ TeslaBlockingItems/Config.cs
@@ -6,9 +6,14 @@
     public sealed class Config : IConfig
     {
         public bool IsEnabled { get; set; } = true;
+        [Description("If true, all the items in the list disable the tesla (when in hand or inventory (OnlyInHands setting)). If false, disabled completely.")]
+        public bool ItemsDisableTesla { get; set; } = true;
         [Description("If set to true, player would need to have the item in hands to disable tesla, otherwise it only needs to be in the inventory.")]
         public bool OnlyInHands { get; set; } = true;
-        [Description("List of the tesla disabling items, default is security guard keycard, but could be any item.")]
+        [Description("List of the tesla disabling items, default is security guard keycard, but could be any item. Don't leave it empty, set ItemsDisableTesla to false.")]
         public ItemType[] DisablingItems { get; set; } = { ItemType.KeycardGuard };
+
+        [Description("If true, SCP 268 (the hat) disables the tesla gates when the user is using the hat.")] 
+        public bool HatDisablesTesla { get; set; } = true;
     }
 }
--- TeslaBlockingItems/Handlers/Player.cs
+++ TeslaBlockingItems/Handlers/Player.cs
@@ -1,4 +1,7 @@
-﻿namespace TeslaBlockingItems.Handlers
+﻿using CustomPlayerEffects;
+using Exiled.API.Features;
+
+namespace TeslaBlockingItems.Handlers
 {
     using Exiled.Events.EventArgs;
     using System.Linq;
@@ -7,31 +10,39 @@
     {
         public void OnTriggeringTesla(TriggeringTeslaEventArgs eventArgs)
         {
-            // Get all disabling items listed in config
-            ItemType[] disablingItemTypes = TeslaKeycardBlocker.Instance.Config.DisablingItems;
-            if (disablingItemTypes.Length == 0)
+            if (TeslaKeycardBlocker.Instance.Config.ItemsDisableTesla)
             {
-                return;
+                // Get all disabling items listed in config
+                ItemType[] disablingItemTypes = TeslaKeycardBlocker.Instance.Config.DisablingItems;
+
+                // Check wether item needs to be in hand or is only required in inventory
+                if (TeslaKeycardBlocker.Instance.Config.OnlyInHands)
+                {
+                    if (disablingItemTypes.Contains<ItemType>(eventArgs.Player.Inventory.curItem))
+                    {
+                        // Disable tesla gate trigger
+                        eventArgs.IsTriggerable = false;
+                    }
+                }
+                else
+                {
+                    // Search inventory of the player for any of the disabling items
+                    if (eventArgs.Player.Inventory.items.Any<Inventory.SyncItemInfo>(item =>
+                        disablingItemTypes.Contains(item.id)))
+                    {
+                        // Disable tesla gate trigger
+                        eventArgs.IsTriggerable = false;
+                    }
+                }
             }
 
-            // Check wether item needs to be in hand or is only required in inventory
-            if (TeslaKeycardBlocker.Instance.Config.OnlyInHands)
+            // If SCP 268 is active, disable tesla trigger
+            if (TeslaKeycardBlocker.Instance.Config.HatDisablesTesla && eventArgs.Player.ReferenceHub.playerEffectsController.GetEffect<Scp268>().Enabled)
             {
-                if (disablingItemTypes.Contains<ItemType>(eventArgs.Player.Inventory.curItem))
-                {
-                    // Disable tesla gate trigger
-                    eventArgs.IsTriggerable = false;
-                }
+                eventArgs.IsTriggerable = false;
             }
-            else
-            {
-                // Search inventory of the player for any of the disabling items
-                if (eventArgs.Player.Inventory.items.Any<Inventory.SyncItemInfo>(item => disablingItemTypes.Contains(item.id)))
-                {
-                    // Disable tesla gate trigger
-                    eventArgs.IsTriggerable = false;
-                }
-            }
+
+            // CASSIE Announcment -> alle x Minuten Statusmeldung über aktuelle Anzahl von Leuten in der Facility nach Klassen sortiert (Facility Scan)
         }
     }
 }
